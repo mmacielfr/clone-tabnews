6a611e3514732ad132bb6c2a66cf99f990d10c30
Matheus Maciel Franco Rodrigues <92557393+mmacielfr@users.noreply.github.com>
2023-07-05 21:14:45 +0000

    Edit prompt

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-let firstName = window.prompt("Por favor, coloque seu nome:");
+let firstName = document.window.prompt("Por favor, coloque seu nome:");
 
 function Home() {
     return <h1>Oi {{firstName}}, aqui é o Matheus.</h1>;
