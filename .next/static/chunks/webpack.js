d98a41502e02669ddfbc6359da5c1d7f45e6e5c3
Matheus Maciel Franco Rodrigues <92557393+mmacielfr@users.noreply.github.com>
2023-07-05 19:41:33 +0000

    Change main text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd8712a25341fcfe"; }
+/******/ 		__webpack_require__.h = function() { return "7100232ecad1eadd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Test 2</h1>;
+    return <h1>Oi Bia! Aqui é o Matheus, estou só passando pra falar que te amo.</h1>;
 }
 
 export default Home;
